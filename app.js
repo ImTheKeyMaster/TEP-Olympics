60c7f6c046a21194bdf33d4c130aee19f21eca5e
ImTheKeyMaster <jrbenning@hotmail.com>
2026-09-26 11:19:53 -0400

    Update About the Event page

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import { collection, doc, initializeFirestore, onSnapshot, persistentLocalCache,
 
 (() => {
   'use strict';
-  const APP_VERSION = '13';
+  const APP_VERSION = '14';
   const firebaseConfig = {
     apiKey: 'AIzaSyBae3zbFxXrNXIj5WSHA_aECq0y7T7M0v0',
     authDomain: 'tep-olympics.firebaseapp.com',
